013175f50892bcb0c7a4c49b08f946a2c801c766
taoqf <tao_qiufeng@126.com>
2020-03-26 17:06:36 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import am1 from '@mmstudio/am000001';
 
-export default function am12(param1: string) {
+/**
+ * @param mm 系统参数
+ * @param page_name 页面名
+ * @param params 传参
+ */
+export default function goto(mm: am1, page_name: string, params: object) {
+	const { navigation } = mm.props;
+	navigation.navigate(page_name, params);
 }
